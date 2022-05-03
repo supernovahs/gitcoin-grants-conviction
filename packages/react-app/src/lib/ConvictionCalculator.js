3510909ae489f577f962d87182fd5f83de9679e8
0xDarni <0xdarni@pm.me>
2022-05-03 12:16:21 -0700

    Add token for load testing

---
+++ packages/react-app/src/lib/ConvictionCalculator.js
@@ -0,0 +1,13 @@
+import { CONVICTION_MULTIPLIER } from "../constants";
+
+const { ethers } = require("ethers");
+
+export const calculateConviction = (amount, days) => {
+  const daysToSeconds = days * 24 * 60 * 60;
+  return truncate(amount + CONVICTION_MULTIPLIER * daysToSeconds * amount, 2);
+};
+
+const truncate = (number, digits) => {
+  const shift = Math.pow(10, digits);
+  return Math.trunc(number * shift) / shift;
+};
