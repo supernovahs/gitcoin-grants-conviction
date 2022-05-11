776483afc704f09d2437a24b6bee8ce51fc7a11c
DanieleSalatti <me@danielesalatti.com>
2022-05-11 11:35:15 -0700

    Merge commit 'b8d8ebea3d82c3fe097bc167dda33064bf063505'

--- packages/react-app/src/components/VoteItem.jsx
+++ packages/react-app/src/components/VoteItem.jsx
@@ -1,37 +1,17 @@
-import { Button, List, Skeleton, Avatar, Statistic } from "antd";
+import { List, Skeleton, Avatar, Checkbox } from "antd";
 import React, { useEffect, useState } from "react";
-import { UnlockOutlined } from "@ant-design/icons";
 import { ethers } from "ethers";
 import axios from "axios";
 
 import { BUIDL_GUIDL_API_ENDPOINT } from "../constants";
 
-const { Countdown } = Statistic;
-export default function VoteItem({ item, tx, readContracts, writeContracts }) {
+export default function VoteItem({ item, onCheckCallback }) {
   const [loading, setLoading] = useState(true);
   const [grantDetails, setGrantDetails] = useState({});
-  let nowInSeconds = Math.floor(new Date().getTime() / 1000);
-  const [canUnstake, setCanUnstake] = useState(nowInSeconds > item.lockedUntil);
 
-  const handleUnstake = voteId => {
-    console.log("tx address", readContracts.GTCStaking.address);
-
-    tx(writeContracts.GTCStaking.releaseTokens(voteId), update => {
-      console.log("📡 Transaction Update:", update);
-      if (update && (update.status === "confirmed" || update.status === 1)) {
-        console.log(" 🍾 Transaction " + update.hash + " finished!");
-        console.log(
-          " ⛽️ " +
-            update.gasUsed +
-            "/" +
-            (update.gasLimit || update.gas) +
-            " @ " +
-            parseFloat(update.gasPrice) / 1000000000 +
-            " gwei",
-        );
-      }
-    });
-  };
+  function onChange(e) {
+    onCheckCallback(item.voteId, e.target.checked);
+  }
 
   const axiosConfig = {
     baseURL: BUIDL_GUIDL_API_ENDPOINT + "/grant",
@@ -57,30 +37,11 @@ export default function VoteItem({ item, tx, readContracts, writeContracts }) {
     <List.Item>
       <Skeleton loading={loading} title={false} active>
         <List.Item.Meta avatar={<Avatar src={grantDetails.img} />} title={grantDetails.title} />
-        <div style={{ float: "right", marginLeft: "16px" }}>
-          <Countdown
-            valueStyle={{ fontSize: "16px" }}
-            title="Time remaining"
-            value={item.lockedUntil.toString() * 1000}
-            format={`D [days], HH:mm:ss`}
-            onFinish={() => {
-              setCanUnstake(true);
-            }}
-          />
-          Amount: {ethers.utils.formatEther(item.amount)} GTC
-        </div>
-        <Button
-          style={{ marginLeft: "16px" }}
-          onClick={() => {
-            handleUnstake(item.voteId);
-          }}
-          type="primary"
-          shape="round"
-          disabled={!canUnstake}
-          icon={<UnlockOutlined key="unstake" />}
-        >
+        <div style={{ float: "right", marginLeft: "16px" }}>Amount: {ethers.utils.formatEther(item.amount)} GTC</div>
+
+        <Checkbox onChange={onChange} style={{ marginLeft: "16px" }}>
           Unstake
-        </Button>
+        </Checkbox>
       </Skeleton>
     </List.Item>
   );
